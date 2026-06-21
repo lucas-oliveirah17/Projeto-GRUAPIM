1deb676dd6ed3dda04ca237dc73bb8cff18e50c7
lucas-oliveirah17 <lucas.oliveirah17@gmail.com>
2026-06-20 22:21:41 -0300

    chore: setup inicial do Spring Boot, PostgreSQL via Docker e configs do git

---
+++ src/main/java/br/edu/ifsp/gruapim/journaling/JournalingApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.gruapim.journaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/gruapim/journaling/JournalingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.gruapim.journaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
